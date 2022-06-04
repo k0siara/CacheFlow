5a9a44d71b93b299dd152e1ce852cf728c41a622
Patryk Kosieradzki <patryk.kosieradzki@gmail.com>
2022-06-04 23:12:55 +0200

    Improves build time in gradle.properties, adds buildSrc and spotless base

---
+++ buildSrc/src/main/kotlin/com/patrykkosieradzki/cacheflow/Configuration.kt
@@ -0,0 +1,8 @@
+package com.patrykkosieradzki.cacheflow
+
+object Configuration {
+    private const val majorVersion = 1
+    private const val minorVersion = 0
+    private const val patchVersion = 0
+    const val versionName = "$majorVersion.$minorVersion.$patchVersion"
+}
---
+++ buildSrc/src/main/kotlin/com/patrykkosieradzki/cacheflow/Dependencies.kt
@@ -0,0 +1,20 @@
+package com.patrykkosieradzki.cacheflow
+
+object Versions {
+    internal const val ANDROID_GRADLE_PLUGIN = "7.2.0"
+    internal const val ANDROID_GRADLE_SPOTLESS = "6.3.0"
+    internal const val KOTLIN = "1.6.21"
+
+    internal const val JUNIT = "4.13.2"
+}
+
+object Dependencies {
+    const val androidGradlePlugin =
+        "com.android.tools.build:gradle:${Versions.ANDROID_GRADLE_PLUGIN}"
+    const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.KOTLIN}"
+
+    const val spotlessGradlePlugin =
+        "com.diffplug.spotless:spotless-plugin-gradle:${Versions.ANDROID_GRADLE_SPOTLESS}"
+
+    const val junit = "junit:junit:${Versions.JUNIT}"
+}
